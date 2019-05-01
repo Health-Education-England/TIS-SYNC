87c8b46531b77c1f24f24bd533c74cadbbfa2bec
jayanta2018 <jayanta.saha@hee.nhs.uk>
2019-05-01 22:48:20 +0100

    application.properties file with port 8086

--- src/main/java/uk/nhs/hee/tis/tcselasticsearch/TisTcsEsApplication.java
+++ src/main/java/uk/nhs/hee/tis/tcselasticsearch/TisTcsEsApplication.java
@@ -2,8 +2,23 @@ package uk.nhs.hee.tis.tcselasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Configuration
 @SpringBootApplication
+@EnableWebMvc
+@EnableSpringDataWebSupport
+@PropertySource(
+    {
+        "classpath:/application.properties",
+        //"classpath:/config/profileclientapplication.properties",
+        //"classpath:/config/referenceclientapplication.properties",
+        //"classpath:/config/tcsclientapplication.properties"
+    }
+)
 public class TisTcsEsApplication {
 
 	public static void main(String[] args) {
