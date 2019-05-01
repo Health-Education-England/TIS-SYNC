169562b76116e89fa4d1f4eff8a697ecc203fb11
jayanta2018 <jayanta.saha@hee.nhs.uk>
2019-05-01 21:49:10 +0100

    Initial commit for TCS elastic search

---
+++ src/main/java/uk/nhs/hee/tis/tcselasticsearch/TisTcsEsApplication.java
@@ -0,0 +1,13 @@
+package uk.nhs.hee.tis.tcselasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TisTcsEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TisTcsEsApplication.class, args);
+	}
+
+}
