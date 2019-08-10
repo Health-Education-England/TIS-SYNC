b4c60653650e830b12f7749be1f2662a038b5007
JosephKelly <joseph.kelly@hee.nhs.uk>
2019-08-10 11:45:40 +0100

    Added a job to invoke the PersonOwnerRebuild stored procedure
    It is an offline job that can run from the sync service

---
+++ src/main/java/uk/nhs/tis/sync/job/PersonOwnerRebuildJob.java
@@ -0,0 +1,72 @@
+package uk.nhs.tis.sync.job;
+
+import java.util.concurrent.CompletableFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import com.google.common.base.Stopwatch;
+import com.transformuk.hee.tis.tcs.service.repository.PersonRepository;
+import net.javacrumbs.shedlock.core.SchedulerLock;
+
+@Component
+@ManagedResource(objectName = "sync.mbean:name=PersonOwnerRebuildJob",
+    description = "Job that clears and recreates the PersonOwner table")
+public class PersonOwnerRebuildJob {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PersonOwnerRebuildJob.class);
+  private static final int FIFTEEN_MIN = 15 * 60 * 1000;
+
+  @Autowired
+  private PersonRepository personRepository;
+
+  private Stopwatch mainStopWatch;
+
+  @Scheduled(cron = "${application.cron.personOwnerRebuildJob}")
+  @SchedulerLock(name = "personOwnerRebuildScheduledTask", lockAtLeastFor = FIFTEEN_MIN,
+      lockAtMostFor = FIFTEEN_MIN)
+  @ManagedOperation(description = "Run sync of the PersonOwner table")
+  public void personOwnerRebuildJob() {
+    runSyncJob();
+  }
+
+  protected void runSyncJob() {
+    if (mainStopWatch != null) {
+      LOG.info("Sync job [{}] already running, exiting this execution", getJobName());
+      return;
+    }
+    CompletableFuture.runAsync(this::run);
+  }
+
+  protected void run() {
+    try {
+      LOG.info("Sync [{}] started", getJobName());
+      mainStopWatch = Stopwatch.createStarted();
+      personRepository.buildPersonView();
+      LOG.info("Sync job [{}] finished. Total time taken {} to rebuild the table", getJobName(),
+          mainStopWatch.stop().toString());
+      mainStopWatch = null;
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+      mainStopWatch = null;
+    }
+  }
+
+  private String getJobName() {
+    return "PersonOwner job";
+  }
+
+  @ManagedOperation(description = "Is the Person es sync just currently running")
+  public boolean isCurrentlyRunning() {
+    return mainStopWatch != null;
+  }
+
+  @ManagedOperation(description = "The current elapsed time of the current sync job")
+  public String elapsedTime() {
+    return mainStopWatch != null ? mainStopWatch.toString() : "0s";
+  }
+
+}
