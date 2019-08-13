d2fb598335de1db458a2c9ff7382adc3638b6133
Yafang Deng <hihilary@outlook.com>
2019-08-13 18:38:34 +0100

    Use ParameterizedTest of JUnit 5. Increase version to 1.3.0

--- src/test/java/uk/nhs/tis/sync/api/JobResourceTest.java
+++ src/test/java/uk/nhs/tis/sync/api/JobResourceTest.java
@@ -1,10 +1,13 @@
 package uk.nhs.tis.sync.api;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import uk.nhs.tis.sync.job.*;
@@ -18,7 +21,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@RunWith(SpringRunner.class)
+@DisplayName("JobResource")
+@ExtendWith(SpringExtension.class)
 public class JobResourceTest {
 
   @MockBean
@@ -43,7 +47,7 @@ public class JobResourceTest {
 
   private MockMvc mockMvc;
 
-  @Before
+  @BeforeEach
   public void setup() {
     jobResource = new JobResource(personPlacementEmployingBodyTrustJob,
       personPlacementTrainingBodyTrustJob,
@@ -54,6 +58,7 @@ public class JobResourceTest {
     mockMvc = MockMvcBuilders.standaloneSetup(jobResource).build();
   }
 
+  @DisplayName("get Status")
   @Test
   public void shouldReturnAllStatusWhenGetStatus() throws Exception{
     when(personPlacementEmployingBodyTrustJob.isCurrentlyRunning())
@@ -80,20 +85,73 @@ public class JobResourceTest {
       .andExpect(status().isOk());
   }
 
-  @Test
-  public void shouldReturnCorrectStatusWhenTriggerOneIndividualJob() throws Exception {
+  @DisplayName("run a job")
+  @ParameterizedTest(name = "Should return 'just started' status when \"{0}\" is triggered .")
+  @ValueSource(strings = {
+    "personPlacementTrainingBodyTrustJob",
+    "personPlacementEmployingBodyTrustJob",
+    "postEmployingBodyTrustJob",
+    "postTrainingBodyTrustJob",
+    "personElasticSearchSyncJob",
+    "personOwnerRebuildJob"
+  })
+  public void shouldReturnJustStartedWhenAJobTriggered(String name) throws Exception {
+    when(personPlacementTrainingBodyTrustJob.isCurrentlyRunning())
+      .thenReturn(false);
+    when(personPlacementEmployingBodyTrustJob.isCurrentlyRunning())
+      .thenReturn(false);
+    when(postEmployingBodyTrustJob.isCurrentlyRunning())
+      .thenReturn(false);
+    when(postTrainingBodyTrustJob.isCurrentlyRunning())
+      .thenReturn(false);
     when(personElasticSearchSyncJob.isCurrentlyRunning())
       .thenReturn(false);
-    mockMvc.perform(put("/api/job/personElasticSearchSyncJob")
+    when(personOwnerRebuildJob.isCurrentlyRunning())
+      .thenReturn(false);
+
+    mockMvc.perform(put("/api/job/" + name)
       .content(MediaType.APPLICATION_JSON))
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.status").value("just started"));
+  }
 
+
+  @DisplayName("run an already running job")
+  @ParameterizedTest(name = "Should return 'already running' status when trigger a running job \"{0}\".")
+  @ValueSource(strings = {
+    "personPlacementTrainingBodyTrustJob",
+    "personPlacementEmployingBodyTrustJob",
+    "postEmployingBodyTrustJob",
+    "postTrainingBodyTrustJob",
+    "personElasticSearchSyncJob",
+    "personOwnerRebuildJob"
+  })
+  public void shouldReturnAlreadyRunningWhenTriggerARunningJob(String name) throws Exception {
+    when(personPlacementTrainingBodyTrustJob.isCurrentlyRunning())
+      .thenReturn(true);
+    when(personPlacementEmployingBodyTrustJob.isCurrentlyRunning())
+      .thenReturn(true);
+    when(postEmployingBodyTrustJob.isCurrentlyRunning())
+      .thenReturn(true);
+    when(postTrainingBodyTrustJob.isCurrentlyRunning())
+      .thenReturn(true);
+    when(personElasticSearchSyncJob.isCurrentlyRunning())
+      .thenReturn(true);
     when(personOwnerRebuildJob.isCurrentlyRunning())
       .thenReturn(true);
-    mockMvc.perform(put("/api/job/personOwnerRebuildJob")
+
+    mockMvc.perform(put("/api/job/" + name)
       .content(MediaType.APPLICATION_JSON))
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.status").value("already running"));
+  }
+
+  @DisplayName("run a nonexistent job")
+  @Test
+  public void shouldReturnBadRequestWhenTriggeringANonexistentJob() throws Exception {
+    mockMvc.perform(put("/api/job/" + "nonexistentJob")
+      .content(MediaType.APPLICATION_JSON))
+      .andExpect(status().isBadRequest())
+      .andExpect(jsonPath("$.error").value("job not found"));
   }
 }
