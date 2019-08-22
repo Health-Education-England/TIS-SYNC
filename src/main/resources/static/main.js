b02a828539c44c456d6edb960a063daab139b5b2
abrarkhan916 <47320737+abrarkhan916@users.noreply.github.com>
2019-08-22 18:28:07 +0100

    Set up static sync service page with buttons (#25)
    * Sync service page setup
    
    * Bump version

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,61 @@
+function runJob() {
+  const jobName = event.srcElement.id;
+  const url = window.location.origin + window.location.pathname + "api/job/" + jobName;
+
+  const requestParams = {
+    method : "PUT",
+    body : "",
+    headers : { "Content-Type" : "application/json" }
+    };
+
+  fetch(url, requestParams)
+    .then(response => response.json());
+}
+
+function runAllJobs() {
+  const url = window.location.origin + window.location.pathname + "api/jobs/";
+
+  const requestParams = {
+    method : "PUT",
+    body : "",
+    headers : { "Content-Type" : "application/json" }
+    };
+
+  fetch(url, requestParams)
+    .then(response => response.json());
+}
+
+function getStatus() {
+  const url = window.location.origin + window.location.pathname + "api/jobs/status";
+  fetch(url)
+    .then(response => response.json())
+    .then(status => {
+        Object.keys(status).forEach(function(key) {
+          var jobStatus = "Not running";
+          const statusElement = document.getElementById(key + "_status");
+
+          if (status[key]) {
+            jobStatus = "Running";
+            statusElement.classList.add("running-text");
+          }
+          else {
+            statusElement.classList.remove("running-text");
+          }
+
+          statusElement.innerHTML = jobStatus;
+        })
+    });
+}
+
+function registerListeners() {
+  document.getElementById("personOwnerRebuildJob").addEventListener("click", runJob);
+  document.getElementById("personPlacementEmployingBodyTrustJob").addEventListener("click", runJob);
+  document.getElementById("personPlacementTrainingBodyTrustJob").addEventListener("click", runJob);
+  document.getElementById("postEmployingBodyTrustJob").addEventListener("click", runJob);
+  document.getElementById("postTrainingBodyTrustJob").addEventListener("click", runJob);
+  document.getElementById("personElasticSearchSyncJob").addEventListener("click", runJob);
+  document.getElementById("runAllJobs").addEventListener("click", runAllJobs);
+  document.getElementById("getStatus").addEventListener("click", getStatus);
+}
+
+window.addEventListener("load", registerListeners);
