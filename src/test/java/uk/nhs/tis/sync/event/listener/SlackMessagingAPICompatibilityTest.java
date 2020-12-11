2ad134f2e36dcb52668bb7cef9a0d60fbd84af02
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2020-12-11 08:29:44 +0000

    test: Test compatibility of Slack API call
    A rather crude test that the POST call to the Slack API does not
    contain the illegal as_user parameter if this is not populated.
    
    TISNEW-5717

---
+++ src/test/java/uk/nhs/tis/sync/event/listener/SlackMessagingAPICompatibilityTest.java
@@ -0,0 +1,51 @@
+package uk.nhs.tis.sync.event.listener;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import com.hubspot.slack.client.SlackClientFactory;
+import com.hubspot.slack.client.SlackClientRuntimeConfig;
+import com.hubspot.slack.client.methods.params.chat.ChatPostMessageParams;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import com.hubspot.slack.client.SlackClient;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class SlackMessagingAPICompatibilityTest  {
+
+  // we have to scrape the stream output to see what is actually POST'ed to the Slack API
+  private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+
+  @Before
+  public void setStream() {
+    System.setOut(new PrintStream(out));
+  }
+
+  @After
+  public void restoreInitialStream() {
+    System.setOut(originalOut);
+  }
+
+  @Test
+  public void testSlackPostAPICompatibility() {
+
+    String SLACK_TOKEN_NEW_APP = "ANY_TOKEN";
+    SlackClientRuntimeConfig slackClientRuntimeConfig = SlackClientRuntimeConfig.builder().setTokenSupplier(() -> SLACK_TOKEN_NEW_APP).build();
+    SlackClient slackClient = SlackClientFactory.defaultFactory().build(slackClientRuntimeConfig);
+
+    ChatPostMessageParams params = ChatPostMessageParams.builder()
+      .setChannelId("test")
+      .setText("test")
+      .build();
+
+    slackClient.postMessage(params).join();
+
+    Boolean noAsUserParameter = out.toString().indexOf("as_user") == -1;
+
+    assert noAsUserParameter;
+  }
+}
