77d07c1c6926b40d51f3fcc5de055435dddf337b
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-01-21 15:05:00 +0000

    feat: get PostDTO once a message is read and send it to a kinesis stream
    TIS21-1036

---
+++ src/main/java/uk/nhs/tis/sync/config/AmazonKinesisConfiguration.java
@@ -0,0 +1,44 @@
+package uk.nhs.tis.sync.config;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.kinesis.AmazonKinesis;
+import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder;
+import lombok.extern.slf4j.XSlf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.CredentialsProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonKinesisConfiguration {
+
+  @Value("${app.aws.access.key.id}")
+  private String awsAccessKeyId;
+
+  @Value("${app.aws.secret.access.key}")
+  private String awsSecretAccessKey;
+
+  AWSCredentialsProvider credentialsProvider() {
+    String secretFirst10 = StringUtils.isBlank(this.awsSecretAccessKey) ? null : this.awsSecretAccessKey.substring(0, 10);
+    BasicAWSCredentials basic = new BasicAWSCredentials(this.awsAccessKeyId,
+      this.awsSecretAccessKey);
+    return new AWSStaticCredentialsProvider(basic);
+  }
+
+  @Bean
+  public AmazonKinesis amazonKinesis() {
+    AmazonKinesisClientBuilder clientBuilder =  AmazonKinesisClientBuilder.standard();
+    clientBuilder.setRegion("eu-west-2");
+    clientBuilder.setCredentials(credentialsProvider());
+    clientBuilder.setClientConfiguration(new ClientConfiguration());
+
+    return clientBuilder.build();
+  }
+
+}
