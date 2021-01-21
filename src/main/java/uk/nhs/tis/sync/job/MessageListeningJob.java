49df37eae1a752346488ebd393179a95d1521a36
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-01-21 15:05:00 +0000

    feat: get PostDTO once a message is read and send it to a kinesis stream
    TIS21-1036

--- src/main/java/uk/nhs/tis/sync/job/MessageListeningJob.java
+++ src/main/java/uk/nhs/tis/sync/job/MessageListeningJob.java
@@ -1,12 +1,26 @@
 package uk.nhs.tis.sync.job;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+import com.amazonaws.services.kinesis.AmazonKinesis;
+import com.amazonaws.services.kinesis.model.PutRecordsRequest;
+import com.amazonaws.services.kinesis.model.PutRecordsRequestEntry;
+import com.amazonaws.services.kinesis.model.PutRecordsResult;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.model.Message;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.transformuk.hee.tis.tcs.api.dto.PostDTO;
+import com.transformuk.hee.tis.tcs.client.service.impl.TcsServiceImpl;
+import com.transformuk.hee.tis.tcs.service.model.Post;
+import com.transformuk.hee.tis.tcs.service.repository.PostRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -23,9 +37,16 @@ public class MessageListeningJob {
 
   private Stopwatch mainStopWatch;
 
+  @Autowired
+  private PostRepository repository;
+
+  @Autowired
+  private TcsServiceImpl tcsServiceImpl;
+
+  @Autowired
+  private AmazonKinesis amazonKinesis;
+
   @Scheduled(cron = "${application.cron.messageListeningJob}")
-//  @SchedulerLock(name = "messageListeningScheduledTask", lockAtLeastFor = FIFTEEN_MIN,
-//    lockAtMostFor = FIFTEEN_MIN)
   @ManagedOperation(description = "Run MessageListening job")
   public void messageListeningJob() {
     runMessageListeningJob();
@@ -46,33 +67,58 @@ public class MessageListeningJob {
       String queue_url = sqs.getQueueUrl("tis-trainee-sync-queue-preprod").getQueueUrl();
       List<Message> messages = sqs.receiveMessage(queue_url).getMessages();
       for(Message message : messages) {
-        LOG.info(message.toString());
-      }
+        String messageBody = message.getBody();
+        String[] keyValuePairs = messageBody.split(",");
+        String tableName = keyValuePairs[0].split(":")[1];
+        String postId = keyValuePairs[1].split(":")[1];
+        LOG.info(tableName + " " + postId);
 
-//      if (applicationEventPublisher != null) {
-//        applicationEventPublisher
-//          .publishEvent(new JobExecutionEvent(this, "Sync [" + getJobName() + "] started."));
-//      }
-//      mainStopWatch = Stopwatch.createStarted();
-//
-//      personRepository.buildPersonView();
-//
-//      LOG.info("Sync job [{}] finished. Total time taken {} to rebuild the table", getJobName(),
-//        mainStopWatch.stop().toString());
-//      mainStopWatch = null;
-//      if (applicationEventPublisher != null) {
-//        applicationEventPublisher
-//          .publishEvent(new JobExecutionEvent(this, "Sync [" + getJobName() + "] finished."));
-//      }
+        PostDTO postDTO = getPostById(postId);
+
+        sendDataIntoKinesisStream(postDTO);
+      }
     } catch (Exception e) {
       LOG.error(e.getMessage(), e);
       mainStopWatch = null;
-//      if (applicationEventPublisher != null) {
-//        applicationEventPublisher.publishEvent(new JobExecutionEvent(this, "<!channel> Sync ["
-//          + getJobName() + "] failed with exception [" + e.getMessage() + "]."));
-//      }
-//      throw e;
     }
+  }
+
+  private PostDTO getPostById(String id) {
+    PostDTO postDTO = null;
+    try {
+      postDTO = tcsServiceImpl.getPostById(Long.parseLong(id));
+    } catch (Exception e) {
+      LOG.error(e.getMessage(), e);
+    }
+    return postDTO;
+  }
+
+  private void sendDataIntoKinesisStream(PostDTO postDTO) {
+    PutRecordsRequest putRecordsRequest  = new PutRecordsRequest();
+    putRecordsRequest.setStreamName("tis-stage-mysql-cdc-stream");
+    List <PutRecordsRequestEntry> putRecordsRequestEntryList  = new ArrayList<>();
+
+    PutRecordsRequestEntry putRecordsRequestEntry  = new PutRecordsRequestEntry();
+    putRecordsRequestEntry.setData(ByteBuffer.wrap(stringifyPostDto(postDTO).getBytes()));
+    putRecordsRequestEntry.setPartitionKey(String.format("partitionKey-%d", putRecordsRequestEntryList.size()));
+    putRecordsRequestEntryList.add(putRecordsRequestEntry);
+
+    putRecordsRequest.setRecords(putRecordsRequestEntryList);
+    PutRecordsResult putRecordsResult  = amazonKinesis.putRecords(putRecordsRequest);
+    System.out.println("Put Result" + putRecordsResult);
+  }
+
+
+  private String stringifyPostDto(PostDTO postDTO) {
+    ObjectMapper mapper = new ObjectMapper();
+    String stringifiedPostDTO = null;
+    try {
+      String json = mapper.writeValueAsString(postDTO);
+      stringifiedPostDTO = json;
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    return stringifiedPostDTO;
   }
 
   private String getJobName() {
