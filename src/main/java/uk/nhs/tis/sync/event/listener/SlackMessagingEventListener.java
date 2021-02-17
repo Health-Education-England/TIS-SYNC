f167c0400192e12002d772492de6248fe6a6f3f0
JosephKelly <joseph.kelly@hee.nhs.uk>
2021-02-17 00:16:51 +0000

    refactor: only slack when configured with a channel
    This should stop us logging lots of errors in tests.
    
    NO-TICKET

--- src/main/java/uk/nhs/tis/sync/event/listener/SlackMessagingEventListener.java
+++ src/main/java/uk/nhs/tis/sync/event/listener/SlackMessagingEventListener.java
@@ -1,36 +1,38 @@
 package uk.nhs.tis.sync.event.listener;
 
+import com.hubspot.slack.client.SlackClient;
+import com.hubspot.slack.client.methods.params.chat.ChatPostMessageParams;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import com.hubspot.slack.client.SlackClient;
-import com.hubspot.slack.client.methods.params.chat.ChatPostMessageParams;
 import uk.nhs.tis.sync.event.JobExecutionEvent;
 
 @Component
 public class SlackMessagingEventListener {
 
   private static final Logger LOG = LoggerFactory.getLogger(SlackMessagingEventListener.class);
-
-  @Value("${slack.job.notification-channel}")
-  private String CHANNEL_ID = "";
-
   SlackClient slackClient;
+  private String channelId;
 
   @Autowired
-  public SlackMessagingEventListener(SlackClient slackClient) {
+  public SlackMessagingEventListener(SlackClient slackClient,
+      @Value("${slack.job.notification-channel}") String channelId) {
     this.slackClient = slackClient;
+    this.channelId = channelId;
   }
 
   @EventListener
   public void handleJobExecutionEvent(JobExecutionEvent event) {
-    LOG.debug("Received job completion event with message [" + event.getMessage() + "]");
-    ChatPostMessageParams params = ChatPostMessageParams.builder().setChannelId(CHANNEL_ID)
-        .setText(event.getMessage()).build();
-    slackClient.postMessage(params);
+    LOG.debug("Received job completion event with message [{}]", event.getMessage());
+    if (!StringUtils.isBlank(channelId)) {
+      ChatPostMessageParams params = ChatPostMessageParams.builder().setChannelId(channelId)
+          .setText(event.getMessage()).build();
+      slackClient.postMessage(params);
+    }
   }
 
 }
--- src/test/java/uk/nhs/tis/sync/event/listener/SlackMessagingEventListenerTest.java
+++ src/test/java/uk/nhs/tis/sync/event/listener/SlackMessagingEventListenerTest.java
@@ -1,18 +1,19 @@
 package uk.nhs.tis.sync.event.listener;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
+
+import com.hubspot.algebra.Result;
+import com.hubspot.slack.client.SlackClient;
+import com.hubspot.slack.client.models.response.SlackError;
+import com.hubspot.slack.client.models.response.chat.ChatPostMessageResponse;
 import java.util.concurrent.CompletableFuture;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import com.hubspot.algebra.Result;
-import com.hubspot.slack.client.SlackClient;
-import com.hubspot.slack.client.models.response.SlackError;
-import com.hubspot.slack.client.models.response.chat.ChatPostMessageResponse;
 import uk.nhs.tis.sync.event.JobExecutionEvent;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -21,13 +22,15 @@ public class SlackMessagingEventListenerTest {
   @Mock
   SlackClient mockClient;
 
+  @Mock
+  CompletableFuture<Result<ChatPostMessageResponse, SlackError>> mockFuture;
+
   @Test
   public void testSendJobStatus() {
 
     // Given we have a slack API client that will respond to any message post
-    when(mockClient.postMessage(any())).thenReturn(buildMockSlackResultAnswer());
-    SlackMessagingEventListener testClass = new SlackMessagingEventListener(mockClient);
-
+    when(mockClient.postMessage(any())).thenReturn(mockFuture);
+    SlackMessagingEventListener testClass = new SlackMessagingEventListener(mockClient, "channel");
     // When a job completion event is received
     JobExecutionEvent event = new JobExecutionEvent(this, "Test message");
     testClass.handleJobExecutionEvent(event);
@@ -36,9 +39,29 @@ public class SlackMessagingEventListenerTest {
     verify(mockClient).postMessage(any());
   }
 
-  public static CompletableFuture<Result<ChatPostMessageResponse, SlackError>> buildMockSlackResultAnswer() {
-    CompletableFuture<Result<ChatPostMessageResponse, SlackError>> mockResult =
-        mock(CompletableFuture.class);
-    return mockResult;
+  @Test
+  public void testNoSendWhenChannelIsNull() {
+
+    // Given we have a slack API client that will respond to any message post
+    SlackMessagingEventListener testClass = new SlackMessagingEventListener(mockClient, null);
+    // When a job completion event is received
+    JobExecutionEvent event = new JobExecutionEvent(this, "Test message");
+    testClass.handleJobExecutionEvent(event);
+
+    // Then our client should not receive a message.
+    verifyNoInteractions(mockClient);
+  }
+
+  @Test
+  public void testNoSendWhenChannelIsBlank() {
+
+    // Given we have a slack API client that will respond to any message post
+    SlackMessagingEventListener testClass = new SlackMessagingEventListener(mockClient, " ");
+    // When a job completion event is received
+    JobExecutionEvent event = new JobExecutionEvent(this, "Test message");
+    testClass.handleJobExecutionEvent(event);
+
+    // Then our client should not receive a message.
+    verifyNoInteractions(mockClient);
   }
 }
