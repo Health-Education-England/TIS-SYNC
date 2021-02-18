138f1764ae7c3961dd1eaef29d9f7e8c2036a3ee
Andy Dingley <Judge40@users.noreply.github.com>
2021-02-18 14:06:52 +0000

    fix: Exclude null site fields from JSON
    TIS21-1032

--- src/main/java/uk/nhs/tis/sync/dto/SiteDmsDto.java
+++ src/main/java/uk/nhs/tis/sync/dto/SiteDmsDto.java
@@ -1,8 +1,11 @@
 package uk.nhs.tis.sync.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Data;
 
 @Data
+@JsonInclude(Include.NON_NULL)
 public class SiteDmsDto {
 
   private String id;
