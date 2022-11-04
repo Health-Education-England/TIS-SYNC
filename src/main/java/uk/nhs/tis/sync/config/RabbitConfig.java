bc3a9c2538f581a21d4a5904560600af59534556
Yafang Deng <hihilary@outlook.com>
2022-11-04 11:18:10 +0000

    fix:code smell
    TIS21-3229

--- src/main/java/uk/nhs/tis/sync/config/RabbitConfig.java
+++ src/main/java/uk/nhs/tis/sync/config/RabbitConfig.java
@@ -6,7 +6,6 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
